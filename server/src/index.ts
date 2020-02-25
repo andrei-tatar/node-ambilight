42664e56530259f6d803b72b6a58f90db7e79eaa
Andrei Tatar <tatarandrei@gmail.com>
2020-02-25 20:25:38 +0200

    quick gamma correction

--- server/src/index.ts
+++ server/src/index.ts
@@ -104,6 +104,25 @@ const correction$ = config$.pipe(
     distinctUntilChanged((a, b) => isEqual(a, b)),
 );
 
+const GAMMA_LUT = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+    2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+    5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+    90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+    115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+    144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+    177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+    215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+];
+
 const subscription = combineLatest([sampleData$, correction$])
     .pipe(
         map(([data, correction]) => {
@@ -116,7 +135,7 @@ const subscription = combineLatest([sampleData$, correction$])
             return data;
         }),
         scan((acc, frame) => {
-            if (acc?.length != frame.length) {
+            if (acc.length != frame.length) {
                 acc = frame.slice();
             } else {
                 for (let i = 0; i < frame.length; i++) {
@@ -124,7 +143,14 @@ const subscription = combineLatest([sampleData$, correction$])
                 }
             }
             return acc;
-        }, null as Uint8Array | null),
+        }, new Uint8Array()),
+        map(frame => {
+            const clone = frame.slice(0);
+            for (let i = 0; i < clone.length; i++) {
+                clone[i] = GAMMA_LUT[clone[i]];
+            }
+            return clone;
+        }),
         withLatestFrom(ws$),
         switchMap(([data, ws]) => new Promise((resolve, reject) => ws.send(data, err => {
             if (err) reject(err); else resolve();
